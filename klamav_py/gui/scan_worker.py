e2563aaa770fcfc254974d0c788ddc741e296cc1
gradia <gradia@disroot.org>
2026-08-27 00:10:55 +0200

    gui: rimuovi import inutilizzato ScanResult

--- klamav_py/gui/scan_worker.py
+++ klamav_py/gui/scan_worker.py
@@ -14,7 +14,7 @@ from typing import Optional
 
 from PySide6.QtCore import QThread, Signal
 
-from ..clamd_client import ClamdClient, ClamdError, ScanResult
+from ..clamd_client import ClamdClient, ClamdError
 from ..quarantine import Quarantine
 
 
